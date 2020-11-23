a6711d9ec86b8b610d24fc9eb50842ff14113b33
Satya Prasad Behara <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-11-23 07:06:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -295,6 +295,7 @@ if ($(window).width() > 300) {
         $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
       }else{
         $('#navbar_top').removeClass("fixed-top");
+        $('#navbar_top').addClass("fade-in");	      
          // remove padding top from body
         $('body').css('padding-top', '0');
       }   
